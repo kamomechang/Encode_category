70e4963d28d5f7b404a74a4dc546cdd249ee82d3
kamomechang <86882912+kamomechang@users.noreply.github.com>
2021-11-13 22:14:08 +0900

    Add files via upload

---
+++ OneHotEncoding.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+import numpy as np
+import pandas as pd
+
+def main(df_data):
+    
+    
+    df_data=pd.get_dummies(df_data, dummy_na=True, drop_first=True)
+    df_data.to_csv("ohe_encoded_data.csv")
+             
+if __name__=="__main__":
+    
+    df_data=pd.read_csv("Dataset\\marketing_campaign.csv", index_col=0, sep="\t")
+    main(df_data)
